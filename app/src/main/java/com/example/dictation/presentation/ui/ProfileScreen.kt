b3036e66ba23166cdd578768a4bd0c327bc8890e
aysuda <AysudaBeigi@gmail.com>
2023-04-24 02:36:04 +0430

    wip:Redesigns ProfileScreen.kt , SignUpScreen.kt , strings.xml

--- app/src/main/java/com/example/dictation/presentation/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/dictation/presentation/ui/ProfileScreen.kt
@@ -1,8 +1,29 @@
 package com.example.dictation.presentation.ui
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.dictation.R
+import com.example.dictation.domain.User
 
 @Composable
-fun ProfileScreen(){
+fun ProfileScreen(modifier: Modifier, user: User) {
+    Column(modifier = modifier
+        .fillMaxSize()
+        .padding(16.dp)) {
+        Text(text = stringResource(id = R.string.first_name))
+        TextField(value = user.firstName, onValueChange = { }, enabled = false)
+        Spacer(modifier = Modifier.size(32.dp))
+        Text(text = stringResource(id = R.string.last_name))
+        TextField(value = user.lastName, onValueChange = {})
+        Spacer(modifier = Modifier.size(32.dp))
+        Text(text = stringResource(id = R.string.phone_number))
+        TextField(value = user.phoneNumber, onValueChange = { })
+
+    }
 
 }
--- app/src/main/java/com/example/dictation/presentation/ui/SignUpScreen.kt
+++ app/src/main/java/com/example/dictation/presentation/ui/SignUpScreen.kt
@@ -1,9 +1,38 @@
 package com.example.dictation.presentation.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.room.util.TableInfo
+import com.example.dictation.R
 
 @Composable
-fun SignUpScreen(modifier: Modifier){
+fun SignUpScreen(modifier: Modifier,onSignUpClicked:()->Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        val inputValue = remember { mutableStateOf(TextFieldValue()) }
+        Text(text = stringResource(id = R.string.first_name))
+        OutlinedTextField(value = inputValue.value, onValueChange = { inputValue.value = it })
+        Text(text = stringResource(id = R.string.last_name))
+        OutlinedTextField(value = inputValue.value, onValueChange = { inputValue.value = it })
+        Text(text = stringResource(id = R.string.phone_number))
+        OutlinedTextField(value = inputValue.value, onValueChange = { inputValue.value = it })
 
+        Button(onClick = { onSignUpClicked()}) {
+            Text(text = stringResource(id = R.string.sign_up))
+        }
+    }
 }
